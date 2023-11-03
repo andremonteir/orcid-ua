6ae1a764948cb18f9962c6d943b06a10cf47f2d8
André Monteiro <andremonteiro@ua.pt>
2023-11-03 18:33:46 +0000

    2nd

--- src/index.php
+++ src/index.php
@@ -18,44 +18,59 @@
         <pre id="result">...</pre>
         <form action="" method="POST">
 			<br /><br />
-            <input class="form-control" type="text" id="token" placeholder="tokenid">
-            <input class="form-control" type="text" id="search" placeholder="search items" value="demeranville">
+            <input class="form-control" type="text" id="token" name="token" placeholder="tokenid">
+            <input class="form-control" type="text" id="bearer" name="bearer" placeholder="bearer">
+            <input class="form-control" type="text" id="search" name="search" placeholder="search items" value="demeranville">
             <input class="btn btn-info" type="submit" value="pesquisar" name="submit">
 		</form>  
         
         <?php
 		
         $redirectUri = "https://wonderful-pebble-03a26b403.3.azurestaticapps.net";
-        $clientId = "APP-3CNS49SKAR5OGR9E"; 
-        $clientSecret = "a37f23e3-b58f-4a99-9ea6-2820868f6610";
+        $client_id = "APP-3CNS49SKAR5OGR9E"; 
+        $client_secret = "a37f23e3-b58f-4a99-9ea6-2820868f6610";
         $authEndpoint = "https://orcid.org/oauth/authorize/";
         $tokenEndpoint = "https://orcid.org/oauth/token/";
-        $url = "https://qa.orcid.org/oauth/authorize?response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A&client_id=" . $clientId . "&scope=openid";
+        $url = "https://qa.orcid.org/oauth/authorize?response_type=token&redirect_uri=" . redirectUri . $client_id . "&scope=openid";
         // https://qa.orcid.org/oauth/authorize?response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A&client_id=APP-3CNS49SKAR5OGR9E&scope=openid"
+        $searchURL = "https://pub.orcid.org/v1.2/search/orcid-bio/?q=";
 
 		if (isset($_POST['submit']) && $_POST['submit']!="") {
 
             if(isset($_REQUEST['token'])){
 
+                $bearer = $_POST['bearer'];
+                $search = $_POST['search'];
                 $token = $_REQUEST['token'];
-                $url = tokenEndpoint . $token;
+                $url = $searchURL;
                 $client = curl_init($url);
                 
+                $payload = json_encode( array( "Bearer"=> $bearer) );
+                curl_setopt($client, CURLOPT_POSTFIELDS, $payload );
                 curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
                 $response = curl_exec($client);
                 $result = json_decode($response);
                 
+                echo "<table class='table table-striped'>";
+
                 if(isset($result)){
-                    echo "<table class='table table-striped'>";
-                    echo "<tr><td>Order ID:</td><td>$result->order_id</td></tr>";
-                    echo "<tr><td>Amount:</td><td>$result->amount</td></tr>";
-                    echo "<tr><td>Response Code:</td><td>$result->response_code</td></tr>";
-                    echo "<tr><td>Response Desc:</td><td>$result->response_desc</td></tr>";
-                    echo "</table>";
+                   
+                    echo "<tr><td>ORCID ID</td><td>$result->uri</td></tr>";
                 }
+                echo "</table>";
+
             }
             else{
 
+                $url = tokenEndpoint . $token;
+                $client = curl_init($url);
+                $payload = json_encode( array( "client_id"=> $client_id,  "client_secret"=> $client_secret,"grant_type"=> "client_credentials","scope"=> "scope") );
+                curl_setopt($client, CURLOPT_POSTFIELDS, $payload );
+                curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+                $response = curl_exec($client);
+                $result = json_decode($response);
+
+                print_r($result);
             }
 		}
 			
